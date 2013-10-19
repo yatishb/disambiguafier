4fde2835fe4399c40fcf9297dd07efdff1aed94b
Yatish <yatishby@gmail.com>
2013-10-19 19:01:14 +0800

    Trying new method of using preceding and succeding word to train the processor the word w Encountering problem where all the values in arraylist are the same. Unable to determine the reason

--- src/sctrain.java
+++ src/sctrain.java
@@ -7,6 +7,42 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+class surroundingWords {
+	private static String precedingWord;
+	private static String succedingWord;
+	private static int count1;
+	private static int count2;
+	
+	public surroundingWords() {
+		count1 = 0;
+		count2 = 0;
+	}
+	
+	public String getPrecedingWord() {
+		return precedingWord;
+	}
+	public String getSuccedingWord() {
+		return succedingWord;
+	}
+	public int getCount1() {
+		return count1;
+	}
+	public void incrementCount1() {
+		count1++;
+	}
+	public int getCount2() {
+		return count2;
+	}
+	public void incrementCount2() {
+		count2++;
+	}
+	public void setWords(String previous, String next) {
+		precedingWord = previous;
+		succedingWord = next;
+	}
+}
+
+
 public class sctrain {
 	public static final int INITIALIZEZERO = 0;
 	public static final int INVALIDNEGATIVE = -1;
@@ -26,11 +62,15 @@ public class sctrain {
 		return INVALIDNEGATIVE;
 	}
 	
-	public static int searchWord(List<String> precedingWords, String check) {
+	public static int searchWord(List<surroundingWords> featureWords, 
+			String previous, String next) {
 		int i = INITIALIZEZERO;
-		int limit = precedingWords.size();
+		int limit = featureWords.size();
 		while(i<limit){
-			if(precedingWords.get(i).equalsIgnoreCase(check))
+			String precedingWord = featureWords.get(i).getPrecedingWord();
+			String succedingWord = featureWords.get(i).getSuccedingWord();
+			if(precedingWord.equalsIgnoreCase(previous) && 
+					succedingWord.equalsIgnoreCase(next))
 				return i;
 			i++;
 		}
@@ -41,7 +81,9 @@ public class sctrain {
 		int i = INITIALIZEZERO;
 		int limit = words.length;
 		while (i<limit) {
-			if(words[i].contains(PUNCTUATION_APOS) || words[i].contains(PUNCTUATION_COMMA) || words[i].contains(PUNCTUATION_STOP)) {
+			if(words[i].contains(PUNCTUATION_APOS) || 
+					words[i].contains(PUNCTUATION_COMMA) ||
+					words[i].contains(PUNCTUATION_STOP)) {
 				int j=i;
 				for(j=i; j<limit-1; j++) {
 					words[j] = words[j+1]; 
@@ -57,10 +99,7 @@ public class sctrain {
 		String word1 = args[0], word2 = args[1];
 		String trainingFile = args[2], statsOutputFile = args[3];
 		
-		List<String> precedingWords = new ArrayList<String>();
-		List<Integer> bigramCountWord1 = new ArrayList<Integer>();
-		List<Integer> bigramCountWord2 = new ArrayList<Integer>();
-		List<Integer> countPrecedingWords = new ArrayList<Integer>();
+		List<surroundingWords> featureList = new ArrayList<surroundingWords>();
 		int noOfDiffPrecedingWords = INITIALIZEZERO ;
 		
 		File fileValidation = new File(trainingFile);
@@ -73,29 +112,37 @@ public class sctrain {
 		while(input != null) {
 			String[] id = input.split("\t");
 			String[] wordsInArray = id[1].split(" ");
+			String wordBefore = "", wordAfter = "";
 			
 			int index = searchWord(wordsInArray, ">>");
-			
-			String wordBefore = wordsInArray[index-2];
-			int indexIfExists = searchWord(precedingWords, wordBefore);
+			if(index-2 >= 0) {
+				wordBefore= wordsInArray[index-2];
+			}
+			if(index+2 < wordsInArray.length) {
+				wordAfter= wordsInArray[index+2];
+			}
+			int indexIfExists = searchWord(featureList, wordBefore, wordAfter);
 			if(indexIfExists == -1) {
+				featureList.add(new surroundingWords());
+				surroundingWords newFeature = featureList.get(noOfDiffPrecedingWords);
+				newFeature.setWords(wordBefore, wordAfter);
+				if(wordsInArray[index].equalsIgnoreCase(word1)) {
+					newFeature.incrementCount1();
+				} else {
+					newFeature.incrementCount2();
+				}
+				featureList.set(noOfDiffPrecedingWords, newFeature);
 				noOfDiffPrecedingWords ++;
-				precedingWords.add(noOfDiffPrecedingWords-1, wordBefore);
-				if(wordsInArray[index].equalsIgnoreCase(word1)) {
-					bigramCountWord1.add(noOfDiffPrecedingWords - 1, 1);
-					bigramCountWord2.add(noOfDiffPrecedingWords - 1, 0);
-				} else {
-					bigramCountWord1.add(noOfDiffPrecedingWords - 1, 0);
-					bigramCountWord2.add(noOfDiffPrecedingWords - 1, 1);
-				}
+				System.out.print(newFeature.getPrecedingWord()+" "+newFeature.getSuccedingWord()+"\n");
 			} else {
+				System.out.print("Coming here");
+				surroundingWords existingFeature = featureList.get(indexIfExists);
 				if(wordsInArray[index].equalsIgnoreCase(word1)) {
-					int previousCount = bigramCountWord1.get(indexIfExists);
-					bigramCountWord1.set(indexIfExists, previousCount+1);
+					existingFeature.incrementCount1();
 				} else {
-					int previousCount = bigramCountWord2.get(indexIfExists);
-					bigramCountWord2.set(indexIfExists, previousCount+1);
+					existingFeature.incrementCount2();
 				}
+				featureList.set(indexIfExists, existingFeature);
 			}
 			
 			input = fin.readLine();
@@ -105,13 +152,17 @@ public class sctrain {
 		int i= INITIALIZEZERO;		
 		BufferedWriter fout = new BufferedWriter(new FileWriter(statsOutputFile));
 		for(; i<noOfDiffPrecedingWords; i++) {
-			fout.write(precedingWords.get(i));
-			if(bigramCountWord1.get(i) > bigramCountWord2.get(i)) {
-				fout.write(" "+word1+"\n");
+			//System.out.print(i);
+			surroundingWords feature = featureList.get(i);
+			String toPrint = feature.getPrecedingWord()+"\t"+feature.getSuccedingWord()+"\t"+feature.getCount1()+"\t"+feature.getCount2()+"\n";
+			
+			/*if(feature.getCount1() > feature.getCount2()) {
+				toPrint += "\t"+word1+"\n";
 			} else {
-				fout.write(" "+word2+"\n");
-			}
-			System.out.print(precedingWords.get(i)+" "+bigramCountWord1.get(i)+" "+bigramCountWord2.get(i)+"\n");
+				toPrint += "\t"+word2+"\n";
+			}*/
+			fout.write(toPrint);
+			//System.out.print(toPrint);
 		}
 		fout.close();
 	  }

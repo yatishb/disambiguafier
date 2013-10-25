e08b255ba95da074eac20b1999d3792ebbcb912c
Yatish <yatishby@gmail.com>
2013-10-25 08:29:49 +0800

    Final copy (accuracy for the given data sets btw 70-80%): The methods followed are in the following order- 1. C(-2,2) 2. C(-1,1) 3. Surrounding Words upto distance of 5 words 4. Preceding word

--- src/sctest.java
+++ src/sctest.java
@@ -19,6 +19,10 @@ public class sctest {
 	public static final int DIST_SURROUNDING = 5;
 	
 	
+	/*
+	 * Function to search for a particular word in an array of Strings
+	 * and return its index+1
+	 */
 	public static int searchWord(String[] words, String check) {
 		int i = INITIALIZEZERO;
 		int limit = words.length;
@@ -30,21 +34,10 @@ public class sctest {
 		return INVALIDNEGATIVE;
 	}
 	
-	public static int searchWord(List<String> prevWords, List<String> nextWords,
-			String previous, String next) {
-		int i = INITIALIZEZERO;
-		int limit = prevWords.size();
-		while(i<limit){
-			String precedingWord = prevWords.get(i);
-			String succedingWord = nextWords.get(i);
-			if(precedingWord.equalsIgnoreCase(previous) && 
-					succedingWord.equalsIgnoreCase(next))
-				return i;
-			i++;
-		}
-		return INVALIDNEGATIVE;
-	}
-	
+	/*
+	 * Function to remove the punctuation symbols from the array of
+	 * words that form part of the sentence
+	 */
 	public static void removePunctuation(String[] words) {
 		int i = INITIALIZEZERO;
 		int limit = words.length;
@@ -66,6 +59,10 @@ public class sctest {
 		}
 	}
 	
+	/*
+	 * Function that reads the stop words from a file
+	 * and stores them into a list containing all the stop words
+	 */
 	public static void readStopWrds(List<String> stopWords) 
 			throws IOException {
 		BufferedReader fin = new BufferedReader(new FileReader("stopwd.txt"));
@@ -77,6 +74,10 @@ public class sctest {
 		fin.close();
 	}
 	
+	/*
+	 * Function that removes the stop words from the array of words
+	 * that are part of the sentence
+	 */
 	public static void removeStopWords(List<String> stopWords, 
 			String[] words) {
 		int i = INITIALIZEZERO, j = INITIALIZEZERO;
@@ -97,14 +98,76 @@ public class sctest {
 		}
 	}
 	
+	/*
+	 * Function that returns the ambiguous word that is to be used
+	 * when only considering the preceding word to the ambiguous
+	 * word
+	 */
+	private static String extractUsingBigram(String word1, String word2,
+			List<String> bigramPrecedingWord, List<Integer> bigramWord1,
+			List<Integer> bigramWord2, String wordBefore) {
+		int indexOfPreceding;
+		if(bigramPrecedingWord.contains(wordBefore) == true){
+			indexOfPreceding = bigramPrecedingWord.indexOf(wordBefore);
+			if(bigramWord1.get(indexOfPreceding) > 
+			bigramWord2.get(indexOfPreceding)){
+				return word1;
+			} else {
+				return word2;
+			}
+		} else {
+			int num = (int) (Math.random()*2);
+			if(num == 0)
+				return word1;
+			else
+				return word2;
+		}
+	}
+
+	/*
+	 * Function that returns the ambiguous word that matches the 
+	 * collocation C(-1,1) that has been stored in the training/model file
+	 */
+	private static String getCorrectWord(String word1, String word2,
+			List<String> cminus1to1, List<String> correctWord, 
+			String wordBefore, String wordAfter) throws IOException {
+		
+		int indexOfPreceding = cminus1to1.indexOf(wordBefore+" "+wordAfter);
+		if(indexOfPreceding == -1) {
+			return "";			
+		} else {
+			return(correctWord.get(indexOfPreceding));
+		}
+	}
+	
+	/*
+	 * Function that returns the ambiguous word that matches the 
+	 * collocation C(-2,2) that has been stored in the training/model file
+	 */
+	private static String getCorrectWord(String word1, String word2,
+			List<String> cminus2to2, List<String> correctWord, 
+			String wordBefore2, String wordBefore1, String wordAfter1, 
+			String wordAfter2) throws IOException {
+		
+		int indexOfPreceding = cminus2to2.indexOf(wordBefore2+" "+wordBefore1+
+				" "+wordAfter1+" "+wordAfter2);
+		if(indexOfPreceding == -1) {
+			return "";			
+		} else {
+			return(correctWord.get(indexOfPreceding));
+		}
+	}
+	
+	
 	public static void main(String[] args) throws IOException {
 		String word1 = args[0], word2 = args[1];
 		double countWord1 = 0, countWord2 = 0, numCollocation = 0, numSurrounding = 0;
 		String testFile = args[2], statsFile = args[3], answerFile = args[4];
 		
-		List<String> prevWords = new ArrayList<String>();
-		List<String> nextWords = new ArrayList<String>();
-		List<String> correctWord = new ArrayList<String>();
+		List<String> cminus1to1 = new ArrayList<String>();
+		List<String> cminus2to2 = new ArrayList<String>();
+		List<String> correctWordColloc1 = new ArrayList<String>();
+		List<String> correctWordColloc2 = new ArrayList<String>();
 		
 		List<String> bigramPrecedingWord = new ArrayList<String>();
 		List<Integer> bigramWord1 = new ArrayList<Integer>();
@@ -115,6 +178,9 @@ public class sctest {
 		List<Integer> surroundingCount2 = new ArrayList<Integer>();
 		double numSurrounding1 = 0, numSurrounding2 = 0;
 		
+		/*
+		 * Read from the model file and generate the required lists
+		 */
 		File fileValidation = new File(statsFile); 
 		if(!fileValidation.exists()) {
 			System.out.print("No file found");
@@ -134,20 +200,37 @@ public class sctest {
 			} else if(counter == 2) {
 				numCollocation = Integer.parseInt(stat[0]);
 			} else if(counter <= 2+numCollocation) {
+				//acquiring collocation C(-2,2) data
 				stat[0] = stat[0].toLowerCase();
 				stat[1] = stat[1].toLowerCase();
-				prevWords.add(stat[0]);
-				nextWords.add(stat[1]);
-				if(Integer.valueOf(stat[3]) > Integer.valueOf(stat[5])){
-					correctWord.add(stat[2]);
-				} else if(Integer.valueOf(stat[3]) < Integer.valueOf(stat[5])) {
-					correctWord.add(stat[4]);
+				cminus2to2.add(stat[0]+" "+stat[1]+" "+stat[2]+" "+stat[3]);
+				if(Integer.valueOf(stat[5]) > Integer.valueOf(stat[7])){
+					correctWordColloc2.add(stat[4]);
+				} else if(Integer.valueOf(stat[5]) < Integer.valueOf(stat[7])) {
+					correctWordColloc2.add(stat[6]);
 				} else if(countWord1 > countWord2) {
-					correctWord.add(stat[2]);
+					correctWordColloc2.add(stat[4]);
 				} else {
-					correctWord.add(stat[4]);
+					correctWordColloc2.add(stat[6]);
 				}
-
+			} else if(counter == 3+cminus2to2.size()) {
+				numCollocation = Integer.parseInt(stat[0]);				
+			} else if(counter <= 3+cminus2to2.size()+numCollocation) {
+				//acquiring collocation C(-1,1) data
+				stat[0] = stat[0].toLowerCase();
+				stat[1] = stat[1].toLowerCase();
+				cminus1to1.add(stat[0]+" "+stat[1]);
+				if(Integer.valueOf(stat[3]) > Integer.valueOf(stat[5])){
+					correctWordColloc1.add(stat[2]);
+				} else if(Integer.valueOf(stat[3]) < Integer.valueOf(stat[5])) {
+					correctWordColloc1.add(stat[4]);
+				} else if(countWord1 > countWord2) {
+					correctWordColloc1.add(stat[2]);
+				} else {
+					correctWordColloc1.add(stat[4]);
+				}
+				
+				//calculating preceding word data
 				if(bigramPrecedingWord.contains(stat[0]) == false) {
 					bigramPrecedingWord.add(stat[0]);
 					bigramWord1.add(Integer.valueOf(stat[3]));
@@ -157,9 +240,10 @@ public class sctest {
 					bigramWord1.set(index, bigramWord1.get(index)+Integer.valueOf(stat[3]));
 					bigramWord2.set(index, bigramWord2.get(index)+Integer.valueOf(stat[5]));
 				}
-			} else if(counter == 3+numCollocation){
+			} else if(counter == 4+cminus1to1.size()+cminus2to2.size()){
 				numSurrounding = Integer.parseInt(stat[0]);
-			} else if(counter <= 3+numSurrounding+numCollocation){
+			} else if(counter <= 4+cminus1to1.size()+cminus2to2.size()+numCollocation){
+				//Surrounding Words upto distance 5 data
 				stat[0] = stat[0].toLowerCase();
 				surroundingWord.add(stat[0]);
 				surroundingCount1.add(Integer.parseInt(stat[2]));
@@ -182,25 +266,44 @@ public class sctest {
 		List<String> stopWords = new ArrayList<String>();
 		readStopWrds(stopWords);
 				
+		/*
+		 * Start reading the test file and using the interpreted data
+		 * from the model file use the following methods in the order
+		 * of priority:
+		 * 1. Collocation C(-2,2)
+		 * 2. Collocation C(-1,1)
+		 * 3. Surrounding words upto a distance of 5 words
+		 * 4. Preceding words to the ambiguous word
+		 */
 		fin = new BufferedReader(new FileReader(testFile));
 		BufferedWriter fout = new BufferedWriter(new FileWriter(answerFile));
 		input = fin.readLine();
 		while(input != null) {
 			String[] id = input.split("\t");
 			String[] wordsInArray = id[1].split(" ");
-			String wordBefore = null, wordAfter = null;
-			//removePunctuation(wordsInArray);
+			String wordBefore1 = ":", wordAfter1 = ":", wordBefore2 = ":", wordAfter2 = ":";
 			
 			int index = searchWord(wordsInArray, ">>");
 			if(index-2 >= 0) {
-				wordBefore= wordsInArray[index-2].toLowerCase();
+				wordBefore1= wordsInArray[index-2].toLowerCase();
+			}
+			if(index+1 < wordsInArray.length) {
+				wordAfter1= wordsInArray[index+1].toLowerCase();
+			}
+			if(index-3 >= 0) {
+				wordBefore2= wordsInArray[index-3].toLowerCase();
 			}
 			if(index+2 < wordsInArray.length) {
-				wordAfter= wordsInArray[index+1].toLowerCase();
+				wordAfter2= wordsInArray[index+2].toLowerCase();
 			}
 			
-			String word = getCorrectWord(word1, word2, prevWords, 
-					nextWords, correctWord, wordBefore, wordAfter);
+			String word = getCorrectWord(word1, word2, cminus2to2, 
+					correctWordColloc2, wordBefore2, wordBefore1, 
+					wordAfter1, wordAfter2);
+			if(word.equals("")){
+				word = getCorrectWord(word1, word2, cminus1to1, 
+					correctWordColloc1, wordBefore1, wordAfter1);
+			}
 			//removeStopWords(stopWords, wordsInArray);
 			
 			if(word.equals("")){
@@ -212,9 +315,8 @@ public class sctest {
 					int exists = surroundingWord.indexOf(wordsInArray[newIndex-i].toLowerCase());
 					if(exists != -1 && surroundingCount1.get(exists)!=0 
 							&& surroundingCount2.get(exists)!=0) {
-						prob1 *= (surroundingCount1.get(exists)/numSurrounding1);
-						prob2 *= surroundingCount2.get(exists)/numSurrounding2;
-						System.out.println(surroundingWord.get(exists));
+						prob1 *= (surroundingCount1.get(exists)/countWord1);
+						prob2 *= surroundingCount2.get(exists)/countWord2;
 					}
 					i++;
 				}
@@ -224,13 +326,11 @@ public class sctest {
 					int exists = surroundingWord.indexOf(wordsInArray[newIndex+i].toLowerCase());
 					if(exists != -1 && surroundingCount1.get(exists)!=0 
 							&& surroundingCount2.get(exists)!=0) {
-						prob1 *= (surroundingCount1.get(exists)/numSurrounding1);
-						prob2 *= surroundingCount2.get(exists)/numSurrounding2;
-						System.out.println(surroundingWord.get(exists));
+						prob1 *= (surroundingCount1.get(exists)/countWord1);
+						prob2 *= surroundingCount2.get(exists)/countWord2;
 					}
 					i++;
 				}
-				
 				if(prob1>prob2){
 					word = word1;
 				} else if(prob2>prob1) {
@@ -238,53 +338,14 @@ public class sctest {
 				} else {
 					word = extractUsingBigram(word1, word2,
 							bigramPrecedingWord, bigramWord1, bigramWord2,
-							wordBefore);
+							wordBefore1);
 				}
-				System.out.println(id[0]+ " "+prob1+" "+prob2);
 			}
 			fout.write(id[0] + " " + word + "\n");
-			
-			
 			input = fin.readLine();
 		}
-		
 		fin.close();
 		fout.close();
 	  }
 
-	private static String extractUsingBigram(String word1, String word2,
-			List<String> bigramPrecedingWord, List<Integer> bigramWord1,
-			List<Integer> bigramWord2, String wordBefore) {
-		int indexOfPreceding;
-		if(bigramPrecedingWord.contains(wordBefore) == true){
-			indexOfPreceding = bigramPrecedingWord.indexOf(wordBefore);
-			if(bigramWord1.get(indexOfPreceding) > 
-			bigramWord2.get(indexOfPreceding)){
-				return word1;
-			} else {
-				return word2;
-			}
-		} else {
-			/*int num = (int) (Math.random()*2);
-			if(num == 0)
-				return word1;
-			else
-				return word2;*/
-			return "";
-		}
-	}
-
-	private static String getCorrectWord(String word1, String word2,
-			List<String> prevWords, List<String> nextWords,
-			List<String> correctWord, String wordBefore, String wordAfter) 
-					throws IOException {
-		
-		int indexOfPreceding = searchWord(prevWords, nextWords, 
-				wordBefore, wordAfter);
-		if(indexOfPreceding == -1) {
-			return "";			
-		} else {
-			return(correctWord.get(indexOfPreceding));
-		}
-	}
 }
--- src/sctrain.java
+++ src/sctrain.java
@@ -21,7 +21,10 @@ public class sctrain {
 	public static final CharSequence PUNCTUATION_UNK = "`";
 	public static final int DIST_SURROUNDING = 5;
 	
-	
+	/*
+	 * Function to search for a particular word in an array of Strings
+	 * and return its index+1
+	 */
 	public static int searchWord(String[] words, String check) {
 		int i = INITIALIZEZERO;
 		int limit = words.length;
@@ -33,21 +36,10 @@ public class sctrain {
 		return INVALIDNEGATIVE;
 	}
 	
-	public static int searchWord(List<String> prevWords, List<String> nextWords,
-			String previous, String next) {
-		int i = INITIALIZEZERO;
-		int limit = prevWords.size();
-		while(i<limit){
-			String precedingWord = prevWords.get(i);
-			String succedingWord = nextWords.get(i);
-			if(precedingWord.equalsIgnoreCase(previous) && 
-					succedingWord.equalsIgnoreCase(next))
-				return i;
-			i++;
-		}
-		return INVALIDNEGATIVE;
-	}
-	
+	/*
+	 * Function to remove the punctuation symbols from the array of
+	 * words that form part of the sentence
+	 */
 	public static void removePunctuation(Vector<String> words) {
 		int i = INITIALIZEZERO;
 		int limit = words.size();
@@ -68,6 +60,10 @@ public class sctrain {
 		}
 	}
 	
+	/*
+	 * Function that reads the stop words from a file
+	 * and stores them into a list containing all the stop words
+	 */
 	public static void readStopWrds(List<String> stopWords) 
 			throws IOException {
 		BufferedReader fin = new BufferedReader(new FileReader("stopwd.txt"));
@@ -79,6 +75,10 @@ public class sctrain {
 		fin.close();
 	}
 	
+	/*
+	 * Function that removes the stop words from the array of words
+	 * that are part of the sentence
+	 */
 	public static void removeStopWords(List<String> stopWords, 
 			Vector<String> words) {
 		int i = INITIALIZEZERO, j = INITIALIZEZERO;
@@ -95,115 +95,91 @@ public class sctrain {
 			}
 		}
 	}
-	
-	public static void main(String[] args) throws IOException {
-		String word1 = args[0], word2 = args[1];
-		String trainingFile = args[2], statsOutputFile = args[3];
-		int countWord1 = 0, countWord2 = 0;
-		
-		List<String> prevWords = new ArrayList<String>();
-		List<String> nextWords = new ArrayList<String>();
-		List<Integer> count1 = new ArrayList<Integer>();
-		List<Integer> count2 = new ArrayList<Integer>();
-		
-		//read Stop word file and update stop word list
-		List<String> stopWords = new ArrayList<String>();
-		readStopWrds(stopWords);
-		
-		List<String> surroundingWord = new ArrayList<String>();
-		List<Integer> surroundingCount1 = new ArrayList<Integer>();
-		List<Integer> surroundingCount2 = new ArrayList<Integer>();
-		
-		File fileValidation = new File(trainingFile);
-		if(!fileValidation.exists()) {
-			System.out.print("No file found");
-			return;
-		}
-		BufferedReader fin = new BufferedReader(new FileReader(trainingFile));
-		String input = fin.readLine();
-		while(input != null) {
-			String[] id = input.split("\t");
-			String[] wordsInArray = id[1].split(" ");
-			Vector<String> words = new Vector<String>();
-			String wordBefore = "", wordAfter = "";
-			
-			int i=0;
-			for(; i<wordsInArray.length;i++){
-				words.add(wordsInArray[i]);
-			}
-			
-			removePunctuation(words);
-			
-			int index = words.indexOf(">>")+1;
-			if(index-2 >= 0) {
-				wordBefore= words.get(index-2).toLowerCase();
-			}
-			if(index+2 < words.size()) {
-				wordAfter= words.get(index+2).toLowerCase();
-			}
-			int indexIfExists = searchWord(prevWords, nextWords, wordBefore, wordAfter);
-			if(indexIfExists == -1) {
-				nextWords.add(wordAfter);
-				prevWords.add(wordBefore);
-				if(words.get(index).equalsIgnoreCase(word1)) {
-					count1.add(1);
-					count2.add(0);
-					countWord1++;
-					removeStopWords(stopWords, words);
-					extractSurroundingWords(surroundingWord, surroundingCount1,
-							surroundingCount2, words,1);
-				} else {
-					count1.add(0);
-					count2.add(1);
-					countWord2++;
-					removeStopWords(stopWords, words);
-					extractSurroundingWords(surroundingWord, surroundingCount1,
-							surroundingCount2, words,2);
-				}
-			} else {
-				if(words.get(index).equalsIgnoreCase(word1)) {
-					count1.set(indexIfExists, count1.get(indexIfExists)+1);
-					countWord1++;
-					removeStopWords(stopWords, words);
-					extractSurroundingWords(surroundingWord, surroundingCount1,
-							surroundingCount2, words,1);
-				} else {
-					count2.set(indexIfExists, count2.get(indexIfExists)+1);
-					countWord2++;
-					removeStopWords(stopWords, words);
-					extractSurroundingWords(surroundingWord, surroundingCount1,
-							surroundingCount2, words,2);
-				}
-			}
-			
-			
-			input = fin.readLine();
-		}
-		
-		fin.close();
-		int i= INITIALIZEZERO;		
-		BufferedWriter fout = new BufferedWriter(new FileWriter(statsOutputFile));
-		String toPrint = word1+" "+countWord1+"\n";
-		fout.write(toPrint);
-		toPrint = word2+" "+countWord2+"\n";
-		fout.write(toPrint);
-		fout.write(prevWords.size()+"\n");
-		for(; i<prevWords.size(); i++) {
-			toPrint = prevWords.get(i)+" "+nextWords.get(i);
-			toPrint += " "+word1+" "+count1.get(i)+" "+word2+" "+count2.get(i)+"\n";
-			
-			fout.write(toPrint);
-		}
-		fout.write(surroundingWord.size()+"\n");
-		for(i= INITIALIZEZERO; i<surroundingWord.size(); i++) {
-			toPrint = surroundingWord.get(i)+" "+word1+" "+surroundingCount1.get(i);
-			toPrint += " "+word2+" "+surroundingCount2.get(i)+"\n";
-			
-			fout.write(toPrint);
-		}
-		fout.close();
-	  }
 
+	/*
+	 * Function that generates the required data for collocation
+	 * C(-1, 1)
+	 */
+	private static void collocMinus1To1(String word1, List<String> cminus1to1,
+			List<Integer> count1, List<Integer> count2, Vector<String> words, int index) {
+
+		String wordBefore = "", wordAfter = ""; 
+		if(index-2 >= 0) {
+			wordBefore= words.get(index-2).toLowerCase();
+		}
+		if(index+2 < words.size()) {
+			wordAfter= words.get(index+2).toLowerCase();
+		}
+		String checkString = wordBefore+" "+wordAfter;
+		int indexIfExists = cminus1to1.indexOf(checkString);
+		if(indexIfExists == -1) {
+			cminus1to1.add(checkString);
+			if(words.get(index).equalsIgnoreCase(word1)) {
+				count1.add(1);
+				count2.add(0);
+			} else {
+				count1.add(0);
+				count2.add(1);
+			}
+		} else {
+			if(words.get(index).equalsIgnoreCase(word1)) {
+				count1.set(indexIfExists, count1.get(indexIfExists)+1);
+			} else {
+				count2.set(indexIfExists, count2.get(indexIfExists)+1);
+			}
+		}
+	}
+	
+	/*
+	 * Function that generates the required data for collocation
+	 * C(-2, 2)
+	 */
+	private static void collocMinus2To2(String word1, List<String> cminus2to2,
+			List<Integer> count1, List<Integer> count2, Vector<String> words, 
+			int index) {
+		String checkString = ""; 
+		if(index-3 >= 0) {
+			checkString= words.get(index-3).toLowerCase();
+		} else {
+			checkString = ":";
+		}
+		if(index-2 >= 0) {
+			checkString += " "+words.get(index-2).toLowerCase();
+		} else {
+			checkString +=" :";
+		}
+		if(index+2 < words.size()) {
+			checkString += " "+words.get(index+2).toLowerCase();
+		} else {
+			checkString += " :";
+		}
+		if(index+3 < words.size()) {
+			checkString += " "+words.get(index+3).toLowerCase();
+		} else {
+			checkString += " :";
+		}
+		int indexIfExists = cminus2to2.indexOf(checkString);
+		if(indexIfExists == -1) {
+			cminus2to2.add(checkString);
+			if(words.get(index).equalsIgnoreCase(word1)) {
+				count1.add(1);
+				count2.add(0);
+			} else {
+				count1.add(0);
+				count2.add(1);
+			}
+		} else {
+			if(words.get(index).equalsIgnoreCase(word1)) {
+				count1.set(indexIfExists, count1.get(indexIfExists)+1);
+			} else {
+				count2.set(indexIfExists, count2.get(indexIfExists)+1);
+			}
+		}
+	}
+
+	/*
+	 * Function that extracts the surrounding words from the training set
+	 */
 	private static void extractSurroundingWords(List<String> surroundingWord,
 			List<Integer> surroundingCount1, List<Integer> surroundingCount2,
 			Vector<String> words, int num) {
@@ -252,4 +228,100 @@ public class sctrain {
 			i++;
 		}
 	}
+
+	
+	public static void main(String[] args) throws IOException {
+		String word1 = args[0], word2 = args[1];
+		String trainingFile = args[2], statsOutputFile = args[3];
+		int countWord1 = 0, countWord2 = 0;
+		
+		List<String> cminus1to1 = new ArrayList<String>();
+		List<String> cminus2to2 = new ArrayList<String>();
+		List<Integer> count1Colloc1 = new ArrayList<Integer>();
+		List<Integer> count2Colloc1 = new ArrayList<Integer>();
+		List<Integer> count1Colloc2 = new ArrayList<Integer>();
+		List<Integer> count2Colloc2 = new ArrayList<Integer>();
+		
+		
+		//read Stop word file and update stop word list
+		List<String> stopWords = new ArrayList<String>();
+		readStopWrds(stopWords);
+		
+		List<String> surroundingWord = new ArrayList<String>();
+		List<Integer> surroundingCount1 = new ArrayList<Integer>();
+		List<Integer> surroundingCount2 = new ArrayList<Integer>();
+		
+		File fileValidation = new File(trainingFile);
+		if(!fileValidation.exists()) {
+			System.out.print("No file found");
+			return;
+		}
+		BufferedReader fin = new BufferedReader(new FileReader(trainingFile));
+		String input = fin.readLine();
+		while(input != null) {
+			String[] id = input.split("\t");
+			String[] wordsInArray = id[1].split(" ");
+			Vector<String> words = new Vector<String>();
+			
+			int i=0;
+			for(; i<wordsInArray.length;i++){
+				words.add(wordsInArray[i]);
+			}
+			
+			/*
+			 * Remove punctuations and then determine the values for
+			 * the various collocations C(-1,1) and C(-2,2)
+			 */
+			removePunctuation(words);
+			
+			int index = words.indexOf(">>")+1;
+			collocMinus1To1(word1, cminus1to1, count1Colloc1, count2Colloc1, words, index);
+			collocMinus2To2(word1, cminus2to2, count1Colloc2, count2Colloc2, words, index);
+			
+			if(words.get(index).equalsIgnoreCase(word1)) {
+				countWord1++;
+				removeStopWords(stopWords, words);
+				extractSurroundingWords(surroundingWord, surroundingCount1,
+					surroundingCount2, words,1);
+			} else {
+				countWord2++;
+				removeStopWords(stopWords, words);
+				extractSurroundingWords(surroundingWord, surroundingCount1,
+					surroundingCount2, words,2);
+			}
+			
+			input = fin.readLine();
+		}
+		fin.close();
+		
+		//Start writing onto model file
+		int i= INITIALIZEZERO;		
+		BufferedWriter fout = new BufferedWriter(new FileWriter(statsOutputFile));
+		String toPrint = word1+" "+countWord1+"\n";
+		fout.write(toPrint);
+		toPrint = word2+" "+countWord2+"\n";
+		fout.write(toPrint);
+		fout.write(cminus2to2.size()+"\n");
+		for(; i<cminus2to2.size(); i++) {
+			toPrint = cminus2to2.get(i);
+			toPrint += " "+word1+" "+count1Colloc2.get(i)+" "+word2+" "+count2Colloc2.get(i)+"\n";
+			
+			fout.write(toPrint);
+		}
+		fout.write(cminus1to1.size()+"\n");
+		for(i= INITIALIZEZERO; i<cminus1to1.size(); i++) {
+			toPrint = cminus1to1.get(i);
+			toPrint += " "+word1+" "+count1Colloc1.get(i)+" "+word2+" "+count2Colloc1.get(i)+"\n";
+			
+			fout.write(toPrint);
+		}
+		fout.write(surroundingWord.size()+"\n");
+		for(i= INITIALIZEZERO; i<surroundingWord.size(); i++) {
+			toPrint = surroundingWord.get(i)+" "+word1+" "+surroundingCount1.get(i);
+			toPrint += " "+word2+" "+surroundingCount2.get(i)+"\n";
+			
+			fout.write(toPrint);
+		}
+		fout.close();
+	  }
 }
